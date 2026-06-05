225e42dca8f7b02e81fc2b6bf5c75cfd8a5cf33f
ismaelmarot <43220852+ismaelmarot@users.noreply.github.com>
2026-06-05 19:54:58 -0300

    refact

--- frontend/src/features/dashboard/components/DashboardCard/DashboardCard.tsx
+++ frontend/src/features/dashboard/components/DashboardCard/DashboardCard.tsx
@@ -1,13 +1,13 @@
-import { useDashboardCard } from "./useDashboardCard";
-import { Container, Label, Value } from "./DashboardCard.styles";
+import { useDashboardCard } from './useDashboardCard'
+import { Container, Label, Value } from './DashboardCard.styles'
 
 export const DashboardCard = () => {
-  const { label, value } = useDashboardCard();
+  const { label, value } = useDashboardCard()
 
   return (
     <Container>
       <Label>{label}</Label>
       <Value>{value}</Value>
     </Container>
-  );
-};
+  )
+}
